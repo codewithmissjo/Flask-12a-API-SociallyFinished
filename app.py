0c1b07c6101e9ce4be0a3e71a8421a9e8f7e9e4a
joannawritescode <joannawritescode@gmail.com>
2025-03-09 11:55:21 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+likes = 0
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/api/like', methods=['POST'])
+def stuff():
+  global likes
+  newlike = request.get_json()
+  print(newlike)
+  likes = likes + 1
+  return 'Success', 200
+
+@app.route('/api/get/errything', methods=["GET"])
+def stuff2():
+  print(jsonify(likes))
+  return jsonify(likes)
+
+if __name__ == "__main__":
+  app.run()
---
+++ static/script.js
@@ -0,0 +1,33 @@
+$("#likeBtn").on("click", function(){
+  // POST fetch
+  fetch("/api/like",
+    {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      //make sure to serialize your JSON body
+      body: JSON.stringify("1")
+    }
+  ).then( response => {
+      console.log(response)
+      // CALL THE NEW FUNCTION
+      getLikes()
+    }
+  )
+});
+
+// THE NEW FUNCTION
+function getLikes() {
+  // GET fetch request
+  fetch("/api/get/errything")
+  .then( response => {
+    return response.json();
+  }).then(
+    // update the HTML
+    data => {
+      $(".badge").html(data);
+    }
+  );
+}
