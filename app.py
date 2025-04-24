51dbcc3c9db1122b1ee808a25b2b12ca946953c7
Miss Jo <7433673+codewithmissjo@users.noreply.github.com>
2025-04-24 17:00:59 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ def home():
 @app.route('/api/like', methods=['POST'])
 def stuff():
   global likes
-  newlike = request.get_json()
-  print(f'total likes: ${likes}')
-  likes = likes + 1
+  #newlike = request.get_json()
+  print(f'total likes (POST): {likes}')
+  likes += 1
   return 'Success', 200
 
 @app.route('/api/get/errything', methods=["GET"])
 def stuff2():
-  print(jsonify(likes))
+  print(f'total likes (GET): {likes}')
   return jsonify(likes)
