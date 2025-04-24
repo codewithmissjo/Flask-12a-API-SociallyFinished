bbbef959fcd820adf19cf0e71d0149db74685b77
Miss Jo <7433673+codewithmissjo@users.noreply.github.com>
2025-04-24 17:05:28 -0500

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,21 +1,25 @@
-$("#likeBtn").on("click", function(){
-  // POST fetch
-  fetch("/api/like",
-    {
-      method: "post",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      //make sure to serialize your JSON body
-      body: JSON.stringify("1")
-    }
-  ).then( response => {
-      console.log(response)
-      // CALL THE NEW FUNCTION
-      getLikes()
-    }
-  )
+$(document).ready(function(){
+  getLikes();
+
+  $("#likeBtn").on("click", function(){
+    // POST fetch
+    fetch("/api/like",
+      {
+        method: "post",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        //make sure to serialize your JSON body
+        body: JSON.stringify("1")
+      }
+    ).then( response => {
+        console.log(response)
+        // CALL THE NEW FUNCTION
+        getLikes()
+      }
+    )
+  });
 });
 
 // THE NEW FUNCTION
